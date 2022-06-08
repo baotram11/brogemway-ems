12f82672c36ecffd5af9605427ad10bedc5717e0
baotram11 <19127588@student.hcmus.edu.vn>
2022-06-08 22:24:49 +0700

    Modify Homepage

--- frontend/ems/src/Pages/home.js
+++ frontend/ems/src/Pages/home.js
@@ -1,73 +1,153 @@
 import React from 'react';
+import { BsPlusLg, BsCartPlus, BsCartCheck } from 'react-icons/bs';
+import { IoHeartOutline } from "react-icons/io5";
+
 import Header from '../Layouts/header';
+
+const Products = require('../Database/Products.json');
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
-{/* 
-      <div class="row">
-        {Products.map((product) => (
-          <div
-            className="product"
-            key={product.ProID}
-            class="col d-flex align-items-stretch"
-          >
-            <div class="card">
-              <a href={`/infoProduct/${product.ProID}`}>
-                <img
-                  class="card-img-center img-fluid rounded"
-                  src={require(`../Assets/Images/products/${product.ProID}/1.png`)}
-                />
-                <h5>{product.ProName}</h5>
-              </a>
-              <p color="green">{product.Price}</p>
-            </div>
-          </div>
-        ))}
-      </div>
 
-      <div class="row">
-        {Products.map((product) => (
-          <div
-            className="product"
-            key={product.ProID}
-            class="col d-flex align-items-stretch"
-          >
-            <div class="card">
-              <a href={`/infoProduct/${product.ProID}`}>
-                <img
-                  class="card-img-center img-fluid rounded"
-                  src={require(`../Assets/Images/products/${product.ProID}/1.png`)}
-                />
-                <h5>{product.ProName}</h5>
-              </a>
-              <p color="green">{product.Price}</p>
+      <section class="page-section bg-light" id="portfolio">
+        <div class="container">
+          <div class="text-center">
+            <h2 class="section-heading text-uppercase mb-3 pt-5">Sản phẩm</h2>
+            <h3 class="section-subheading text-muted mb-4">Bao gồm các sản phẩm thuộc thiết bị số, thời trang, giày và túi xách</h3>
+          </div>
+          <div class="row">
+            <div class="col-lg-4 col-sm-6 mb-4">
+              {/* <!-- Portfolio item 1--> */}
+              <div class="portfolio-item">
+                <a class="portfolio-link" href="/products">
+                  <div class="portfolio-hover">
+                    <div class="portfolio-hover-content"><BsPlusLg size={56} /></div>
+                  </div>
+                  <img class="img-fluid" src={require(`../Assets/Images/portfolio/1.png`)} alt="..." />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading">Thiết bị số</div>
+                  <div class="portfolio-caption-subheading text-muted">Điện thoại, máy tính</div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4">
+              {/* <!-- Portfolio item 2--> */}
+              <div class="portfolio-item">
+                <a class="portfolio-link" href="/products">
+                  <div class="portfolio-hover">
+                    <div class="portfolio-hover-content"><BsPlusLg size={56} /></div>
+                  </div>
+                  <img class="img-fluid" src={require(`../Assets/Images/portfolio/2.png`)} alt="..." />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading">Giày tây</div>
+                  <div class="portfolio-caption-subheading text-muted">Giày tây nam</div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4">
+              {/* <!-- Portfolio item 3--> */}
+              <div class="portfolio-item">
+                <a class="portfolio-link" href="/products">
+                  <div class="portfolio-hover">
+                    <div class="portfolio-hover-content"><BsPlusLg size={56} /></div>
+                  </div>
+                  <img class="img-fluid" src={require(`../Assets/Images/portfolio/3.png`)} alt="..." />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading">Túi xách</div>
+                  <div class="portfolio-caption-subheading text-muted">Túi xách thời trang</div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
+              {/* <!-- Portfolio item 4--> */}
+              <div class="portfolio-item">
+                <a class="portfolio-link" href="/products">
+                  <div class="portfolio-hover">
+                    <div class="portfolio-hover-content"><BsPlusLg size={56} /></div>
+                  </div>
+                  <img class="img-fluid" src={require(`../Assets/Images/portfolio/4.png`)} alt="..." />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading">Áo</div>
+                  <div class="portfolio-caption-subheading text-muted">Áo thun, áo sơ mi, áo khoác</div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4 mb-sm-0">
+              {/* <!-- Portfolio item 5--> */}
+              <div class="portfolio-item">
+                <a class="portfolio-link" href="/products">
+                  <div class="portfolio-hover">
+                    <div class="portfolio-hover-content"><BsPlusLg size={56} /></div>
+                  </div>
+                  <img class="img-fluid" src={require(`../Assets/Images/portfolio/5.png`)} alt="..." />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading">Quần</div>
+                  <div class="portfolio-caption-subheading text-muted">Quần tây, quần jean</div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+              {/* <!-- Portfolio item 6--> */}
+              <div class="portfolio-item">
+                <a class="portfolio-link" href="/products">
+                  <div class="portfolio-hover">
+                    <div class="portfolio-hover-content"><BsPlusLg size={56} /></div>
+                  </div>
+                  <img class="img-fluid" src={require(`../Assets/Images/portfolio/6.png`)} alt="..." />
+                </a>
+                <div class="portfolio-caption">
+                  <div class="portfolio-caption-heading">Giày thể thao</div>
+                  <div class="portfolio-caption-subheading text-muted">Giày thể thao nam, nữ</div>
+                </div>
+              </div>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      </section>
 
-      <div class="row">
-        {Products.map((product) => (
-          <div
-            className="product"
-            key={product.ProID}
-            class="col d-flex align-items-stretch"
-          >
-            <div class="card">
-              <a href={`/infoProduct/${product.ProID}`}>
-                <img
-                  class="card-img-center img-fluid rounded"
-                  src={require(`../Assets/Images/products/${product.ProID}/1.png`)}
-                />
-                <h5>{product.ProName}</h5>
-              </a>
-              <p color="green">{product.Price}</p>
+      <section class="page-section bg-light" id="trending">
+        <div class="container">
+
+          <div class="text-center">
+            <h2 class="section-heading text-uppercase mb-3 pt-5">Trending</h2>
+          </div>
+
+          {/* <div class="row">
+            <div class="col-md-12">
+              <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
+
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <div class="row">
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> */}
+
+          <div id="slider" >
+            <div class="carousel">
+
             </div>
           </div>
-        ))}
-      </div> */}
+
+        </div>
+      </section>
 
     </div>
   );
