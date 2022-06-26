00549f632cbfe0edded6f33efa6d5bd712b6bb74
baotram11 <19127588@student.hcmus.edu.vn>
2022-06-26 23:15:53 +0700

    setup the backend
    create server, connect server to database

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
-const routes = require('./routes');
-const setupDB = require('./utils/db');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-import Products from './data';
+require('dotenv').config();
 
 const app = express();
-
-app.get('api/products', (req, res) => {
-	res.send(Products.data);
-});
-
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+// connect the database
+const uri = process.env.DATABASE_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('EMS database connection established successfully!');
+});
+
 app.listen(port, () => {
-	console.log(`Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
