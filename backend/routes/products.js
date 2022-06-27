5a298be7cb7d7dc2b67a92f4ab6ba23868ac1a7f
baotram11 <19127588@student.hcmus.edu.vn>
2022-06-27 17:22:09 +0700

    connect frontend to backend, to GET data
    using api get all products, accounts and categories

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -8,4 +8,30 @@ router.route('/').get((req, res) => {
         .catch((err) => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    Product.findById(req.params.id)
+        .then((products) => res.json(products))
+        .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const proID = req.params.ProID;
+    const proName = req.params.ProName;
+    const price = req.params.Price;
+    const description = req.params.Description;
+    const catID = req.params.CatID;
+
+    const newProduct = new Product({
+        proID,
+        proName,
+        price,
+        description,
+        catID,
+    });
+    console.log(newProduct);
+    newProduct
+        .save()
+        .then(() => res.json('New Product added!'))
+        .catch((err) => res.status(400).json('Error: ' + err));
+});
 module.exports = router;
--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import AccountScreen from '../screens/AccountScreen/AccountScreen';
+import CategoryScreen from '../screens/CategoryScreen/CategoryScreen';
 
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
@@ -9,16 +11,18 @@ import ProductScreen from '../screens/ProductScreen/ProductScreen';
 import RegisterScreen from '../screens/RegisterScreen/RegisterScreen';
 
 const Router = () => {
-	return (
-		<Routes>
-			<Route path='*' element={<NotFoundScreen />} />
-			<Route path='/' element={<HomeScreen />} />
-			<Route path='/login' element={<LoginScreen />} />
-			<Route path='/register' element={<RegisterScreen />} />
-			<Route path='/products' element={<ProductListScreen />} />
-			<Route path='/products/:id' element={<ProductScreen />} />
-		</Routes>
-	);
+    return (
+        <Routes>
+            <Route path='*' element={<NotFoundScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/login' element={<LoginScreen />} />
+            <Route path='/register' element={<RegisterScreen />} />
+            <Route path='/account' element={<AccountScreen />} />
+            <Route path='/categories' element={<CategoryScreen />} />
+            <Route path='/products' element={<ProductListScreen />} />
+            <Route path='/products/:id' element={<ProductScreen />} />
+        </Routes>
+    );
 };
 
 export default Router;
