f5bb3eea9237bdd3c54cee687e3333c60f862b93
baotram11 <19127588@student.hcmus.edu.vn>
2022-08-03 11:50:20 +0700

    use searchParams and find regex case insensitive

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -2,11 +2,11 @@ const Product = require('../models/product.model');
 
 const SearchController = {
 	searchProByName: async (req, res, next) => {
-		console.log(req.query.q);
 		const param = req.query.q;
+
 		try {
 			const result = await Product.find({
-				ProName: { $regex: param, $options: 'i' },
+				ProName: { $regex: param.replaceAll('%', ' '), $options: 'i' },
 			});
 			if (!result) {
 				return res.status(404).json({ error: 'Product does not exist!' });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+import { CookiesProvider } from 'react-cookie';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+	<Provider store={store}>
+		<CookiesProvider>
+			<App />
+		</CookiesProvider>
+	</Provider>
 );
