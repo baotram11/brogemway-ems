eed9276463db0b6fc3109d735e4dcff7d520b476
baotram11 <19127588@student.hcmus.edu.vn>
2022-08-05 08:06:50 +0700

    change directory structure

--- backend/routes/search.router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const SearchController = require('../controllers/search.controller');
-
-router.get('/product', SearchController.searchProByName);
-
-module.exports = router;
